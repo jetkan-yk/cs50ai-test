1c061a68471a632e8c5f55abaf98abff4b9eedf0
Jet Kan <jetkan.yk@gmail.com>
2021-08-08 21:20:53 +0800

    Update knight test

--- puzzle_test.py
+++ puzzle_test.py
@@ -8,10 +8,14 @@ Make sure that this file is in the same directory as puzzle.py!
 """
 from puzzle import *
 
+# source: https://edstem.org/us/courses/176/discussion/107478
+
 
 def test_puzzle0():
-    assert model_check(knowledge0, AKnight) == False
+    # AKnave
     assert model_check(knowledge0, AKnave) == True
+
+    assert model_check(knowledge0, AKnight) == False
     assert model_check(knowledge0, BKnight) == False
     assert model_check(knowledge0, BKnave) == False
     assert model_check(knowledge0, CKnight) == False
@@ -19,27 +23,33 @@ def test_puzzle0():
 
 
 def test_puzzle1():
-    assert model_check(knowledge1, AKnight) == False
+    # AKnave, BKnight
     assert model_check(knowledge1, AKnave) == True
     assert model_check(knowledge1, BKnight) == True
+
+    assert model_check(knowledge1, AKnight) == False
     assert model_check(knowledge1, BKnave) == False
     assert model_check(knowledge1, CKnight) == False
     assert model_check(knowledge1, CKnave) == False
 
 
 def test_puzzle2():
-    assert model_check(knowledge2, AKnight) == False
+    # AKnave, BKnight
     assert model_check(knowledge2, AKnave) == True
     assert model_check(knowledge2, BKnight) == True
+
+    assert model_check(knowledge2, AKnight) == False
     assert model_check(knowledge2, BKnave) == False
     assert model_check(knowledge2, CKnight) == False
     assert model_check(knowledge2, CKnave) == False
 
 
 def test_puzzle3():
+    # AKnight, BKnave, CKnight
     assert model_check(knowledge3, AKnight) == True
-    assert model_check(knowledge3, AKnave) == False
-    assert model_check(knowledge3, BKnight) == False
     assert model_check(knowledge3, BKnave) == True
     assert model_check(knowledge3, CKnight) == True
+
+    assert model_check(knowledge3, AKnave) == False
+    assert model_check(knowledge3, BKnight) == False
     assert model_check(knowledge3, CKnave) == False
