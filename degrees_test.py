0bcc8ceafe3684da5c77ac505b9dc542048ad99f
Jet Kan <jetkan.yk@gmail.com>
2021-08-08 21:13:23 +0800

    Update degrees test

--- degrees_test.py
+++ degrees_test.py
@@ -10,14 +10,58 @@ from degrees import load_data, person_id_for_name, shortest_path
 
 load_data("large")
 
+# source: https://edstem.org/us/courses/176/discussion/226814
 
-def test_two_degrees():
+
+def test0():
     source = person_id_for_name("Jennifer Lawrence")
     target = person_id_for_name("Tom Hanks")
     assert len(shortest_path(source, target)) == 2
 
 
-def test_three_degrees():
+def test1():
     source = person_id_for_name("Emma Watson")
     target = person_id_for_name("Jennifer Lawrence")
     assert len(shortest_path(source, target)) == 3
+
+
+def test_zero_degree():
+    source = person_id_for_name("Tim Zinnemann")
+    target = person_id_for_name("Lahcen Zinoun")
+    assert shortest_path(source, target) is None
+
+
+def test_one_degree():
+    source = person_id_for_name("Tom Cruise")
+    target = person_id_for_name("Lea Thompson")
+    assert len(shortest_path(source, target)) == 1
+
+
+def test_two_degree():
+    source = person_id_for_name("Tom Cruise")
+    target = person_id_for_name("Tom Hanks")
+    assert len(shortest_path(source, target)) == 2
+
+
+def test_three_degree():
+    source = person_id_for_name("Emma Watson")
+    target = person_id_for_name("Jennifer Lawrence")
+    assert len(shortest_path(source, target)) == 3
+
+
+def test_four_degree():
+    source = person_id_for_name("Fred Astaire")
+    target = person_id_for_name("Mohamed Zinet")
+    assert len(shortest_path(source, target)) == 4
+
+
+def test_six_degree():
+    source = person_id_for_name("Juliane Banse")
+    target = person_id_for_name("Bruce Davison")
+    assert len(shortest_path(source, target)) == 6
+
+
+def test_eight_degree():
+    source = person_id_for_name("Juliane Banse")
+    target = person_id_for_name("Julian Acosta")
+    assert len(shortest_path(source, target)) == 8
