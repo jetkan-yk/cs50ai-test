c34df9966a680e3f9e663f420715922286cddc54
Jet Kan <jetkan.yk@gmail.com>
2021-08-11 17:43:46 +0800

    Update puzzle test

--- puzzle_test.py
+++ puzzle_test.py
@@ -8,48 +8,46 @@ Make sure that this file is in the same directory as puzzle.py!
 """
 from puzzle import *
 
-# source: https://edstem.org/us/courses/176/discussion/107478
-
 
 def test_puzzle0():
     # AKnave
-    assert model_check(knowledge0, AKnave) == True
+    assert model_check(knowledge0, AKnave) is True
 
-    assert model_check(knowledge0, AKnight) == False
-    assert model_check(knowledge0, BKnight) == False
-    assert model_check(knowledge0, BKnave) == False
-    assert model_check(knowledge0, CKnight) == False
-    assert model_check(knowledge0, CKnave) == False
+    assert model_check(knowledge0, AKnight) is False
+    assert model_check(knowledge0, BKnight) is False
+    assert model_check(knowledge0, BKnave) is False
+    assert model_check(knowledge0, CKnight) is False
+    assert model_check(knowledge0, CKnave) is False
 
 
 def test_puzzle1():
     # AKnave, BKnight
-    assert model_check(knowledge1, AKnave) == True
-    assert model_check(knowledge1, BKnight) == True
+    assert model_check(knowledge1, AKnave) is True
+    assert model_check(knowledge1, BKnight) is True
 
-    assert model_check(knowledge1, AKnight) == False
-    assert model_check(knowledge1, BKnave) == False
-    assert model_check(knowledge1, CKnight) == False
-    assert model_check(knowledge1, CKnave) == False
+    assert model_check(knowledge1, AKnight) is False
+    assert model_check(knowledge1, BKnave) is False
+    assert model_check(knowledge1, CKnight) is False
+    assert model_check(knowledge1, CKnave) is False
 
 
 def test_puzzle2():
     # AKnave, BKnight
-    assert model_check(knowledge2, AKnave) == True
-    assert model_check(knowledge2, BKnight) == True
+    assert model_check(knowledge2, AKnave) is True
+    assert model_check(knowledge2, BKnight) is True
 
-    assert model_check(knowledge2, AKnight) == False
-    assert model_check(knowledge2, BKnave) == False
-    assert model_check(knowledge2, CKnight) == False
-    assert model_check(knowledge2, CKnave) == False
+    assert model_check(knowledge2, AKnight) is False
+    assert model_check(knowledge2, BKnave) is False
+    assert model_check(knowledge2, CKnight) is False
+    assert model_check(knowledge2, CKnave) is False
 
 
 def test_puzzle3():
     # AKnight, BKnave, CKnight
-    assert model_check(knowledge3, AKnight) == True
-    assert model_check(knowledge3, BKnave) == True
-    assert model_check(knowledge3, CKnight) == True
+    assert model_check(knowledge3, AKnight) is True
+    assert model_check(knowledge3, BKnave) is True
+    assert model_check(knowledge3, CKnight) is True
 
-    assert model_check(knowledge3, AKnave) == False
-    assert model_check(knowledge3, BKnight) == False
-    assert model_check(knowledge3, CKnave) == False
+    assert model_check(knowledge3, AKnave) is False
+    assert model_check(knowledge3, BKnight) is False
+    assert model_check(knowledge3, CKnave) is False
